e6f33e3414eec9a623e67115fe5f4bf4ba99540f
Koloina <k.ranjohanison@etechconsulting-mg.com>
2023-07-25 14:48:00 +0300

    second commits for hubspot integration

--- index.js
+++ index.js
@@ -8,24 +8,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please include the private app access token in your repo BUT only an access token built in a TEST ACCOUNT. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = 'pat-eu1-f73f79ce-5d7d-4517-94cd-23d5b9222ed4';
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const pets = 'https://app-eu1.hubspot.com/contacts/141057371/objects/';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(pets, { headers });
+        const data = resp.data.results;
+        res.render('contacts', { title: 'Contacts | Pets', data });      
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
 app.get('/contacts', async (req, res) => {
     const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
     const headers = {
@@ -41,7 +44,7 @@ app.get('/contacts', async (req, res) => {
     }
 });
 
-* * App.post sample
+// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 app.post('/update', async (req, res) => {
     const update = {
         properties: {
@@ -64,6 +67,14 @@ app.post('/update', async (req, res) => {
     }
 
 });
+
+/** 
+* * This is sample code to give you a reference for how you should structure your calls. 
+
+
+
+* * App.post sample
+
 */
 
 
