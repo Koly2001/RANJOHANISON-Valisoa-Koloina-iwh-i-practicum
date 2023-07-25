2b1463823fe448df24a19eec422eb5b98f8c3af9
Koloina <k.ranjohanison@etechconsulting-mg.com>
2023-07-25 17:20:48 +0300

    Fourth commit for Integrating Hubspot

--- index.js
+++ index.js
@@ -12,61 +12,83 @@ const PRIVATE_APP_ACCESS = 'pat-eu1-f73f79ce-5d7d-4517-94cd-23d5b9222ed4';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder
 
+// Route GET pour la page d'accueil
 app.get('/', async (req, res) => {
-    const pets = 'https://app-eu1.hubspot.com/contacts/141057371/objects/';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(pets, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | Pets', data });      
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    // Faites une requête GET à l'API HubSpot pour récupérer la liste des Custom Objects
+    const response = await axios.get('https://api.hubapi.com/crm/v3/objects/contacts', {
+      headers: {
+        Authorization: `Bearer ${'pat-eu1-f73f79ce-5d7d-4517-94cd-23d5b9222ed4'}`
+      }
+    });
+
+    // Récupérez les Custom Objects à partir de la réponse de l'API
+    const customObjects = response.data.results;
+
+    res.render('homepage', { customObjects });
+  } catch (error) {
+    console.error('Erreur lors de la récupération des Custom Objects depuis HubSpot :', error);
+    // Gérez l'erreur appropriée ici (affichage d'un message d'erreur, etc.)
+    res.render('homepage', { customObjects: [] }); // En cas d'erreur, envoyez une liste vide
+  }
 });
+  
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
+app.get('/update-cobj/:name?', async (req, res) => {
+    const objName = req.params.name;
+    let objData = {};
+  
+    if (objName) {
+
+      const response = await fetch(`http://localhost:3000/api/custom-objects/${objName}`);
+      objData = await response.json();
     }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
+  
+    res.render('updates', {
+      pageTitle: 'Mettre à jour le formulaire d\'objet personnalisé | Integrating With HubSpot I Practicum',
+      objData,
+    });
+  });
+  
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
+// Route POST pour la mise à jour/création de l'objet personnalisé
+app.post('/update-cobj', async (req, res) => {
+    const formData = {
+      id: req.body.id,
+      Name: req.body.Name,
+      Type: req.body.Type,
+      Age: req.body.Age,
+      // Ajoutez plus de champs si nécessaire
     };
+  
+    // Votre code pour gérer les données et les requêtes HTTP avec Axios
+    try {
+      if (formData.id) {
+        await axios.patch(`https://api.hubapi.com/crm/v3/objects/contacts/${formData.id}`, formData, {
+          headers: {
+            Authorization: `Bearer ${'pat-eu1-f73f79ce-5d7d-4517-94cd-23d5b9222ed4'}`,
+            'Content-Type': 'application/json'
+          }
+        });
+      } else {
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
+        await axios.post('https://api.hubapi.com/crm/v3/objects/contacts', formData, {
+          headers: {
+            Authorization: `Bearer ${'pat-eu1-f73f79ce-5d7d-4517-94cd-23d5b9222ed4'}`,
+            'Content-Type': 'application/json'
+          }
+        });
+      }
+  
+      res.redirect('/');
+    } catch (error) {
+      console.error('Erreur lors de la mise à jour/création de l\'objet personnalisé :', error);
     }
-
-});
+  });
+  ;
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
